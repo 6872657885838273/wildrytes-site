031fbb2b26a81413266ab35bb860e1a12842fc7f
DHANUSRI MAHALINGAM <111843665+6872657885838273@users.noreply.github.com>
2025-01-08 02:09:43 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'ap-south-1_S3SBENlJa', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '5ip823on4t821t23t9s07pg2dm', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '1s2t4k2nd1297642v78v9v0ei3', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
