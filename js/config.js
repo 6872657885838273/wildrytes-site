1fdd4999646f01459a465f6a0bf59ce145a8f692
DHANUSRI MAHALINGAM <111843665+6872657885838273@users.noreply.github.com>
2025-01-08 01:09:37 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,6 +2,7 @@ window._config = {
     cognito: {
         userPoolId: 'ap-south-1_c8AWVnpjH', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '7qa8v51rdpjt4jd741dhaqf5u9', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        Clientsecret: '1ktgj7paasso9eh8u8m70vkjl2aavlu9aqodqkbfnvj3c8nlnjdu',
         region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
